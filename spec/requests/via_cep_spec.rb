8413e7f32f53b9afc7939af9bc3f25a5c23e3382
Samuel Rossato <samuel.rossato.lopes@gmail.com>
2024-08-19 13:43:59 -0300

    removendo sonar

--- spec/requests/via_cep_spec.rb
+++ spec/requests/via_cep_spec.rb
@@ -2,14 +2,11 @@ describe "API VIA_CEP" do
     context "/ GET", :consultar_cep do
         before(:all) do
             @ceps = CEPS['ceps']
-            nome = "Tests"
         end
 
         it "Consultar cep aleatório" do
             response = ViaCep.consulta_cep(@ceps.sample)
             expect(response.code).to eq 200
-            puts response.code
-            puts "teste de status code"
             expect(response.body).to match_json_schema("response_body/response_cep")
         end
 
@@ -22,21 +19,8 @@ describe "API VIA_CEP" do
         end
 
         it "Tentar consultar cep inválido" do
-            estados = ""
-            teste = novoteste
             response = ViaCep.consulta_cep("0000")
             expect(response.code).to eq 400
         end
-
-        def teste_metodo
-            
-        end
-
-        if (true)
-            return false
-
-            while test
-                case test
-            end
     end
 end
